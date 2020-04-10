6a1554dd47452bbfde45f3fad92dfa3800465bd8
sawardekar <sanketsawardekar97@gmail.com>
2020-04-10 11:12:46 +0530

    delete api working

--- hrm/urls.py
+++ hrm/urls.py
@@ -8,11 +8,11 @@ router.register(r'type', views.EmployeeTypeViewSet, basename="type")
 router.register(r'product', views.ProductViewSet, basename="product")
 
 urlpatterns = [
-    path('index', views.index, name='index'),
+    path('', views.index, name='index'),
     path('employee', views.employee_table, name="employee_table"),
     path('employee/', views.employee_form, name="employee_create"),
     path('employee/<int:pk>', views.employee_form, name="employee_modified"),
-    path('', include(router.urls)),
+    path('viewset/', include(router.urls)),
     path('emp/', EmployeeDetail.as_view(), name="emp_record"),
     path('emp/<int:pk>/', EmployeeDetail.as_view(), name="emp_modified"),
 ]
