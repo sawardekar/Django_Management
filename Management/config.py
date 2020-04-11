30885e8e04a696c5508beea89f60319410b627d7
sawardekar <sanketsawardekar97@gmail.com>
2020-04-11 19:59:27 +0530

    remove email credential

---
+++ Management/config.py
@@ -0,0 +1,16 @@
+DATABASE = {
+    'NAME': 'management',
+    'USER': 'postgres',
+    'PASSWORD': 'postgres',
+    'HOST': '127.0.0.1',
+    'PORT': '5432'
+}
+
+EMAIL_CREDENTIAL = {
+    'USER': '',
+    'PASSWORD': ''
+}
+SERVER_CREDENTIAL = {
+    'IP_ADDRESS': '127.0.0.1',
+    'PORT': '8000'
+}
